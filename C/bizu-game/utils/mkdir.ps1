38ea8a93a8ee59e35eaca73f18ee754cdaed287b
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-27 00:19:03 -0300

    Delete C/bizu-game/utils directory

--- C/bizu-game/utils/mkdir.ps1
+++
@@ -1,7 +0,0 @@
-param([Parameter(ValueFromRemainingArguments=$true)][String[]]$Entries)
-
-foreach ($Entry in $Entries) {
-    if (-Not (Test-Path $Entry)) {
-        New-Item -ItemType Directory -Name $Entry
-    }
-}
--- C/bizu-game/utils/rm.ps1
+++
@@ -1,7 +0,0 @@
-param([Parameter(ValueFromRemainingArguments=$true)][String[]]$Entries)
-
-foreach ($Entry in $Entries) {
-    if (Test-Path $Entry) {
-        Remove-Item -Recurse -Force $Entry
-    }
-}
