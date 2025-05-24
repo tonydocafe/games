0bd1ce5cd5b4ef755bf19ec0062fb5343ab5ed9f
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-05-24 19:36:09 -0300

    Update enemy.c

--- C/bizu-game/enemy.c
+++ C/bizu-game/enemy.c
@@ -166,3 +166,4 @@ void mudareta2(player* objeto,player2* objeto2) {
             int x2_dir = objeto2->c[0].x + 55;
             int y2_dir = objeto2->c[0].y + 5;
             int x1_esq = objeto->c[0].x - 35;
+            int y1_esq = objeto->c[0].y;
