d5952f3d1fd0271c29a0f6b8a1250da1204d79d8
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-05-31 17:34:56 -0300

    Update enemy.c

--- C/bizu-game/enemy.c
+++ C/bizu-game/enemy.c
@@ -229,3 +229,4 @@ int movi2(player2* objeto2, int direcao) {
                     objeto2->c[0].y = 380;
                     objeto2->saltando = false;
                     objeto2->subindo = false;
+                    objeto2->direcao = objeto2->direcao_anterior;
