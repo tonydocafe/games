6d3ca81538d54fb4d1cce6b2b641ff5c619ac013
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-05-31 17:33:02 -0300

    Update enemy.c

--- C/bizu-game/enemy.c
+++ C/bizu-game/enemy.c
@@ -225,3 +225,4 @@ int movi2(player2* objeto2, int direcao) {
                 objeto2->altura_atual -= objeto2->gravidade;
                 objeto2->c[0].x -=1;    
                 if (objeto2->altura_atual <= 0) {
+                    objeto2->altura_atual = 0;
