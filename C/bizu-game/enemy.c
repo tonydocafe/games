91bfef5826e23cfef01aeb1d745f0972a4ffd243
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-07 19:41:31 -0300

    Update enemy.c

--- C/bizu-game/enemy.c
+++ C/bizu-game/enemy.c
@@ -278,3 +278,4 @@ int movi2(player2* objeto2, int direcao) {
             objeto2->c[0].x -= 10;
             if (objeto2->chute_contador >= objeto2->chute_duracao  ) {
                 objeto2->a = false;
+                objeto2->chute_contador = 0;
