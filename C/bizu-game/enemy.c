96ddd9242464ee33e0a1f6e52d89f5ea7077fe46
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-07 19:30:57 -0300

    Update enemy.c

--- C/bizu-game/enemy.c
+++ C/bizu-game/enemy.c
@@ -245,3 +245,4 @@ int movi2(player2* objeto2, int direcao) {
             if (objeto2->subindo) {
                 objeto2->c[0].y -= objeto2->velocidade_salto - 20;
                 objeto2->altura_atual += objeto2->velocidade_salto -20;
+                objeto2->c[0].x -= 1;
