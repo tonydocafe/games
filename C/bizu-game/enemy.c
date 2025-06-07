e215e0b9005710a35737038d1283b61f39145219
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-06-07 19:45:02 -0300

    Update enemy.c

--- C/bizu-game/enemy.c
+++ C/bizu-game/enemy.c
@@ -290,3 +290,4 @@ int movi2(player2* objeto2, int direcao) {
                 objeto2->altura_atual += objeto2->velocidade_salto ;
                 if (objeto2->altura_atual >= objeto2->altura_maxima) {
                     objeto2->subindo = false;
+                }
